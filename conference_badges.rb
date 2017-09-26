e29e4ed6e052336180c5b04dbc40b8762d2a78d6
Brandon  Harris <cheeridon@gmail.com>
2017-09-26 18:25:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker name
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator array_of_names
+  badge_messages = []
+  array_of_names.each{|name| badge_messages << badge_maker(name)}
+  badge_messages
+end
